03168c99e879cf8be60bd75166c154dc3f7644bc
JP0715 <parrisjordan1997@gmail.com>
2019-09-12 16:09:00 -0500

    adding files

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,3 +1,36 @@
 from random import randint
 #Guess the number.
 
+guessesTaken = 0
+
+print("Enter a number from 1-10. You get 3 guesses.")
+
+number = str(randint(1, 11))
+
+while guessesTaken < 4:
+    guess = str(input("Enter a guess: "))
+    guessesTaken += 1
+
+    if guess < number:
+        print("Too low.")
+    if guess > number:
+        print("Too high.")
+    if guess == number:
+        break
+
+if guess == number:
+    print("Correct. The number was " + str(number) + '. You guessed the number in ' + 
+    str(guessesTaken) + ' guesses.' )
+    
+if guess != number:
+    print('The number was ' + str(number) + '.')
+
+
+
+
+
+        
+
+
+
+
